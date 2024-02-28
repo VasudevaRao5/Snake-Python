734b5d6622adc1956b996e56f452717a5b64e58d
VasudevaRao5 <160588462+VasudevaRao5@users.noreply.github.com>
2024-02-28 17:09:20 +0530

    Update snakegame.py

--- snakegame.py
+++ snakegame.py
@@ -1,87 +1,13 @@
-# Snake Game in Python using Pygame
+# app.py
 
-import pygame
-import time
-import random
+from flask import Flask, render_template
 
-# Initialize Pygame
-pygame.init()
+app = Flask(__name__)
 
-# Set up the game window
-window_x = 720
-window_y = 480
-game_window = pygame.display.set_mode((window_x, window_y))
-pygame.display.set_caption('GeeksforGeeks Snakes')
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-# Define colors
-black = pygame.Color(0, 0, 0)
-white = pygame.Color(255, 255, 255)
-red = pygame.Color(255, 0, 0)
-green = pygame.Color(0, 255, 0)
-blue = pygame.Color(0, 0, 255)
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8080)
 
-# Initialize snake position and size
-snake_position = [100, 50]
-snake_body = [[100, 50], [90, 50], [80, 50], [70, 50]]
-
-# Initialize fruit position randomly
-fruit_position = [random.randrange(1, (window_x // 10)) * 10, random.randrange(1, (window_y // 10)) * 10]
-fruit_spawn = True
-
-# Set initial direction
-direction = 'RIGHT'
-change_to = direction
-
-# Set snake speed
-snake_speed = 15
-
-# Main game loop
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            quit()
-
-    # Update snake direction based on user input
-    keys = pygame.key.get_pressed()
-    for key in keys:
-        if keys[pygame.K_LEFT]:
-            change_to = 'LEFT'
-        if keys[pygame.K_RIGHT]:
-            change_to = 'RIGHT'
-        if keys[pygame.K_UP]:
-            change_to = 'UP'
-        if keys[pygame.K_DOWN]:
-            change_to = 'DOWN'
-
-    # Update snake position
-    if change_to == 'LEFT':
-        snake_position[0] -= 10
-    if change_to == 'RIGHT':
-        snake_position[0] += 10
-    if change_to == 'UP':
-        snake_position[1] -= 10
-    if change_to == 'DOWN':
-        snake_position[1] += 10
-
-    # Update snake body
-    snake_body.insert(0, list(snake_position))
-    if snake_position == fruit_position:
-        fruit_spawn = False
-    else:
-        snake_body.pop()
-
-    # Spawn new fruit
-    if not fruit_spawn:
-        fruit_position = [random.randrange(1, (window_x // 10)) * 10, random.randrange(1, (window_y // 10)) * 10]
-        fruit_spawn = True
-
-    # Update game window
-    game_window.fill(black)
-    for pos in snake_body:
-        pygame.draw.rect(game_window, green, pygame.Rect(pos[0], pos[1], 10, 10))
-    pygame.draw.rect(game_window, white, pygame.Rect(fruit_position[0], fruit_position[1], 10, 10))
-
-    pygame.display.flip()
-    pygame.display.update()
-    pygame.time.Clock().tick(snake_speed)
